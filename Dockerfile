2de54bfb8a4a2a9d63e7fae388a5192cf3d72ec0
Ranier Bonnici <ranierbonnici@gmail.com>
2026-01-13 12:11:00 +0100

    Add project files.

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+﻿# Build stage
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /app
+
+COPY *.csproj ./
+RUN dotnet restore
+
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Runtime stage
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+
+ENV ASPNETCORE_URLS=http://0.0.0.0:8080
+EXPOSE 8080
+
+COPY --from=build /app/out .
+ENTRYPOINT ["dotnet", "TestRender.dll"]
---
+++ TestRender/Program.cs
@@ -0,0 +1,45 @@
+namespace TestRender
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.WebHost.UseUrls("http://0.0.0.0:" +
+                (Environment.GetEnvironmentVariable("PORT") ?? "8080"));
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello from Render!");
+
+            app.Run();
+
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
