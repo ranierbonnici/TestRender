bd1d62b4888166368ef253fa1aaef368e0d10e07
Ranier Bonnici <ranierbonnici@gmail.com>
2026-01-13 13:07:01 +0100

    Changed program

--- TestRender/Program.cs
+++ TestRender/Program.cs
@@ -5,25 +5,20 @@ namespace TestRender
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             var app = builder.Build();
 
+            // Use Render-provided port
             var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
             app.Urls.Add($"http://0.0.0.0:{port}");
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapControllerRoute(
